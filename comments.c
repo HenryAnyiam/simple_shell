f808d49e55669d6660a3e91e293275bc1bca3a6b
HenryAnyiam <anyiamhenry7@gmail.com>
2023-04-26 17:05:21 -0700

    Test edit

--- comments.c
+++ comments.c
@@ -8,13 +8,15 @@
   */
 int remove_comments(cmd_in *cmd)
 {
-	char c, a, nil = '\0', sp = ' ';
-	int i, len;
+	char c;
+	int i;
+/*	char c, a, nil = '\0', sp = ' ';
+	int i, len;*/
 
-	c = cmd->cmd[0];
+/*	c = cmd->cmd[0];
 	a = cmd->cmd[1];
 	if (((c <= nil) && (c <= sp)) && ((a == '\0') || (a == '\n')))
-		return (-1);
+		return (-1);*/
 	for (i = 0; cmd->cmd[i] != '\0'; i++)
 	{
 		c = cmd->cmd[i];
@@ -25,11 +27,11 @@ int remove_comments(cmd_in *cmd)
 			strcut(cmd, i);
 			break;
 		}
-		if (cmd->cmd[i + 1] == '\0')
+/*		if (cmd->cmd[i + 1] == '\0')
 			if (c < 33)
-				strcut(cmd, i);
+				strcut(cmd, i);*/
 	}
-	len = _strlen(cmd->cmd);
+/*	len = _strlen(cmd->cmd);
 	if (len == 0)
 		return (-1);
 	c = cmd->cmd[len - 1];
@@ -40,7 +42,7 @@ int remove_comments(cmd_in *cmd)
 		strcut(cmd, (len - 1));
 		len = _strlen(cmd->cmd);
 		c = cmd->cmd[len - 1];
-	}
+	}*/
 	return (0);
 }
 
